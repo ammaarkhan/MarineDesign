7560db3b482f92661248ba4eff66c5f68f2733e3
ammaarkhan <ammaarkhan03@gmail.com>
2023-09-03 18:51:45 -0700

    fix last push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/marinedesign/', // replace 'repository-name' with your actual repo name
+  build: {
+    rollupOptions: {
+      output: {
+        // This is a fix to ensure assets and relative paths work correctly on GitHub Pages
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  }
 })
